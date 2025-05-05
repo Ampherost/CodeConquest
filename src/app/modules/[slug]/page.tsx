00e2ae0bb4dd43dfb204a3c588f622865ad4ce23
Ampherost <Ampherost235@gmail.com>
2025-05-05 15:14:19 -0700

    consistent coloring for all pages

--- src/app/modules/[slug]/page.tsx
+++ src/app/modules/[slug]/page.tsx
@@ -1,8 +1,10 @@
 export default function ModuleOverviewPage() {
     return (
-      <div className="p-6">
+      <div className = "min-h-screen dark:bg-zinc-900">
+       <div className="p-6">
         <h1 className="text-2xl font-bold">Module Overview</h1>
         <p>This is where the list of chapters for the selected module will appear.</p>
+       </div>
       </div>
     );
   }
