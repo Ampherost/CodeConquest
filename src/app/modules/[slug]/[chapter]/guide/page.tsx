ed3ffa8b72190f5a1da2e67db182172406a4feb6
Ampherost <Ampherost235@gmail.com>
2025-05-05 16:56:06 -0700

    added a guide and quiz section for each chapter
    TODO: still need to find a way to implement dynamic routing

---
+++ src/app/modules/[slug]/[chapter]/guide/page.tsx
@@ -0,0 +1,10 @@
+export default function GuidePage() {
+    return (
+      <div className = "min-h-screen dark:bg-zinc-900">
+       <div className="p-6">
+        <h1 className="text-2xl font-bold">Guide</h1>
+        <p>This is where the guide will go.</p>
+       </div>
+      </div>
+    );
+  }
