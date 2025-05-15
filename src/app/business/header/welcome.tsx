4da1bf210f8b96b7d719e0203ae3f91cef751bae
Eros846 <karkisarik.2021@gmail.com>
2025-05-14 19:46:25 -0700

    Added welcome component.

---
+++ src/app/business/header/welcome.tsx
@@ -0,0 +1,10 @@
+const welcome = () => {
+  return (
+    <div>
+      <h1 className="font-light text-light ">Welcome to</h1>
+      <h3 className="font-light ">DashBoard</h3>
+    </div>
+  );
+};
+
+export default welcome;
