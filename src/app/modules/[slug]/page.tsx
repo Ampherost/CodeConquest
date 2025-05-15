3bf1af606e948c0df3c57cf7fe51e07a41174b32
Ampherost <Ampherost235@gmail.com>
2025-05-14 21:41:44 -0700

    integrated component based architecture in moduleOverviewPage

--- src/app/modules/[slug]/page.tsx
+++ src/app/modules/[slug]/page.tsx
@@ -1,11 +1,22 @@
+import Header from "@/app/components/Header";
+import Footer from "@/app/components/Footer";
+
 export default function ModuleOverviewPage() {
-    return (
-      <div className = "min-h-screen dark:bg-zinc-900">
-       <div className="p-6">
-        <h1 className="text-2xl font-bold">Module Overview</h1>
+  return (
+    <div className="flex flex-col min-h-screen dark:bg-zinc-900 text-zinc-100">
+      {/* Header */}
+      <Header />
+
+      {/* Page Content */}
+      <main className="flex-grow p-6">
+        <h1 className="text-2xl font-bold mb-2">Module Overview</h1>
         <p>This is where the list of chapters for the selected module will appear.</p>
-       </div>
-      </div>
-    );
-  }
+      </main>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+}
+
   
