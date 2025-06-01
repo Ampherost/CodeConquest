0e6e9181414bd09e0a9b076198a8f889e3b3d9f4
Eros846 <karkisarik.2021@gmail.com>
2025-05-31 21:18:16 -0700

    Added redirect from login to business dashboard.

--- src/app/business/dashboard/page.jsx
+++ src/app/business/dashboard/page.jsx
@@ -1,18 +1,67 @@
-import { redirect } from 'next/navigation'
-import { createClient } from '@/utils/supabase/server'
+"use client";
+import { useRouter } from "next/navigation";
 
-export default async function PrivatePage() 
-{
-  const supabase = await createClient()
+import { createClient } from "@/utils/supabase/client";
 
-  const { data, error } = await supabase.auth.getUser()
+import { useState, useEffect } from "react";
 
-  if (error || !data?.user) 
-  {
-    redirect('/login')
-  }
+import Welcome from "../header/welcome";
+import Invitation from "../header/sendInvitationButton";
+import LogoComponent from "../header/logoComponent";
+import Notifications from "../header/notification";
+import Signout from "../header/signOut";
+import Profile from "../header/userProfile";
+import CurrentApplicants from "../applicantsSection/currentApplicants";
+import PendingApplicants from "../applicantsSection/pendingApplicants";
 
-  return (<p>Business Profile: {data.user.email}</p>)
-}
+const supabase = createClient();
 
+const Page = () => {
+  const router = useRouter();
+  const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    supabase.auth.getUser().then(({ data }) => {
+      if (!data.user) {
+        router.replace("/login");
+      } else {
+        setUser(data.user);
+      }
+    });
+  }, [router]);
+
+  if (!user) return null;
+
+  return (
+    //main page
+    <div className="bg-zinc-900 min-h-screen">
+      <div
+        id="header"
+        className="flex flex-row justify-between items-start w-full
+        px-15 py-4"
+      >
+        <div className="flex flex-row items-center space-x-4">
+          <LogoComponent />
+          <Welcome />
+        </div>
+        <div className="flex flex-row items-center space-x-4">
+          <Profile />
+          <Notifications />
+          <Invitation />
+          <Signout />
+        </div>
+      </div>
+
+      <div id="main" className="space-y-5">
+        <div id="Current Applicants" className="flex flex-row p-7">
+          <CurrentApplicants />
+        </div>
+        <div id="Pending Applicatns" className="flex flex-row p-7">
+          <PendingApplicants />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
