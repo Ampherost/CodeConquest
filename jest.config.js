e67df5b99f8a69331769fbd08f03d618e8bad851
Ampherost <Ampherost235@gmail.com>
2025-06-02 16:37:27 -0700

    updated jest configuration for successful tests

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest', 
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1', // if you use “@/…” imports
-  },
-};
